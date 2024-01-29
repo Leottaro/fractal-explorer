a9358ef8f7e21b731fad319da737009d5bf87e27
Leottaro <leo.hafdane@gmail.com>
2024-01-29 17:54:38 +0100

    reformed SettingsTab style

--- src/component/SettingsTab/SettingsTab.tsx
+++ src/component/SettingsTab/SettingsTab.tsx
@@ -1,20 +1,34 @@
-import { useState, useContext } from 'react';
-import AppContext, { ContextSettings } from '../../context/AppContext';
-
+import { useState } from "react";
 import "./SettingsTab.css";
 
-export default function SettingsTab() {
-
-    const [shown, setShown] = useState(false);
+function SettingsTab() {
+    const [hidden, setHidden] = useState(false);
 
     return (
         <>
-            <svg viewBox="-12 -12 24 24" id="SettingsButton" onClick={() => setShown(!shown)} className={shown ? "shown" : ""}>
-                <path d="M 7 0 L -7 0 M 7 0 L 1 -6 M 7 0 L 1 6" strokeLinecap="round" />
+            <div
+                id="SettingsDiv"
+                className={hidden ? "hidden" : undefined}
+            ></div>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 48"
+                id="SettingsButton"
+                onClick={() => setHidden(!hidden)}
+                className={hidden ? "hidden" : undefined}
+            >
+                <path
+                    className="background"
+                    d="M0 24c0-6 6-12 12-12S24 6 24 0v48c0-6-6-12-12-12S0 30 0 24Z"
+                />
+                <path
+                    className="arrow"
+                    d="M20 24H4M20 24l-8-6M20 24l-8 6"
+                    strokeLinecap="round"
+                    strokeWidth={3}
+                />
             </svg>
-            <div id="SettingsDiv" className={shown ? "shown" : ""}>
-
-            </div>
         </>
-    )
+    );
 }
+export default SettingsTab;
