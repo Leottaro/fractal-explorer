5bd817e4e03d7909b47d2849da2959affc894f2a
Leottaro <leo.hafdane@gmail.com>
2024-01-30 16:11:29 +0100

    SettingsTab reform ...again

--- src/component/SettingsTab/SettingsTab.tsx
+++ src/component/SettingsTab/SettingsTab.tsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import Setting from "./Setting";
 import "./SettingsTab.css";
+import AppContext from "../../context/AppContext";
 
 function SettingsTab() {
+    const { settings, setSettings } = useContext(AppContext);
     const [hidden, setHidden] = useState(false);
 
     return (
@@ -12,20 +15,22 @@ function SettingsTab() {
             ></div>
             <svg
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 48"
+                viewBox="0 0 60 90"
+                fill="none"
                 id="SettingsButton"
                 onClick={() => setHidden(!hidden)}
                 className={hidden ? "hidden" : undefined}
             >
                 <path
                     className="background"
-                    d="M0 24c0-6 6-12 12-12S24 6 24 0v48c0-6-6-12-12-12S0 30 0 24Z"
+                    d="M29.9999 15C14.9999 15 -0.000133991 30 0 45C6.43817e-06 60 14.9999 75 29.9999 75C44.9998 75 60 75 60 90V0C60 15 44.9998 15 29.9999 15Z"
                 />
                 <path
                     className="arrow"
-                    d="M20 24H4M20 24l-8-6M20 24l-8 6"
                     strokeLinecap="round"
-                    strokeWidth={3}
+                    strokeLinejoin="round"
+                    strokeWidth={5}
+                    d="M15.0002 45H45.0001M45.0001 45L30.0002 30M45.0001 45L30.0002 60"
                 />
             </svg>
         </>
