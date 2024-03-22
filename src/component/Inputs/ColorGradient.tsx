b33d557ccbc6e4649c640179c5440590d190def1
Leottaro <leo.hafdane@gmail.com>
2024-03-22 20:42:15 +0100

    colorGradient normally bug-free

--- src/component/Inputs/ColorGradient.tsx
+++ src/component/Inputs/ColorGradient.tsx
@@ -15,6 +15,9 @@ const to256 = (p: number) => Math.round(p * 255);
 const torgb = (color: ColorT) =>
     `rgb(${to256(color.r)},${to256(color.g)},${to256(color.b)}) ${color.t * 100}`;
 function toLinearGradient(colors: ColorT[]): string {
+    if (colors.length == 1) {
+        return torgb(colors[0]).split(" ")[0];
+    }
     return (
         "linear-gradient(90deg, " +
         [...colors]
@@ -35,10 +38,9 @@ export default function ColorGradient() {
     const sliderRef = createRef<HTMLDivElement>();
 
     const deselect = () => {
-        if (selected === undefined) {
-            return;
+        if (selected !== undefined) {
+            selected.element.style.zIndex = "0";
         }
-        selected.element.style.zIndex = "0";
         setDragged(false);
         setSelected(undefined);
     };
@@ -75,24 +77,31 @@ export default function ColorGradient() {
     };
 
     const addThumb = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        if (!sliderRef.current) return;
+        if (!sliderRef.current || selected) return;
         if (!(event.target as HTMLElement).classList.contains("colorGradient")) return;
 
         const sliderLeft = sliderRef.current.getBoundingClientRect().left;
         const sliderWidth = sliderRef.current.getBoundingClientRect().width;
         let newOffset = fromDisplayT((settings.sMouse.x - sliderLeft) / sliderWidth);
+        newOffset = Math.max(0, Math.min(1, newOffset));
 
         const newColors = [...settings.uColors, { r: 0, g: 0, b: 0, t: newOffset }].sort(
             (a, b) => a.t - b.t
         );
         const newColorIndex = newColors.findIndex((color) => color.t === newOffset);
-        const beforeColor = newColors[newColorIndex - 1];
-        const afterColor = newColors[newColorIndex + 1];
-        const percent = (newOffset - beforeColor.t) / (afterColor.t - beforeColor.t);
+        if (newColorIndex == 0) {
+            newColors[newColorIndex] = { ...newColors[newColorIndex + 1], t: newOffset };
+        } else if (newColorIndex == newColors.length - 1) {
+            newColors[newColorIndex] = { ...newColors[newColorIndex - 1], t: newOffset };
+        } else {
+            const beforeColor = newColors[newColorIndex - 1];
+            const afterColor = newColors[newColorIndex + 1];
+            const percent = (newOffset - beforeColor.t) / (afterColor.t - beforeColor.t);
 
-        newColors[newColorIndex].r = afterColor.r * percent + beforeColor.r * (1 - percent);
-        newColors[newColorIndex].g = afterColor.g * percent + beforeColor.g * (1 - percent);
-        newColors[newColorIndex].b = afterColor.b * percent + beforeColor.b * (1 - percent);
+            newColors[newColorIndex].r = afterColor.r * percent + beforeColor.r * (1 - percent);
+            newColors[newColorIndex].g = afterColor.g * percent + beforeColor.g * (1 - percent);
+            newColors[newColorIndex].b = afterColor.b * percent + beforeColor.b * (1 - percent);
+        }
         setSettings({ ...settings, uColors: newColors });
     };
 
@@ -106,13 +115,15 @@ export default function ColorGradient() {
                 overflow: "visible",
             }}
             onClick={addThumb}
+            onContextMenu={(event) => event.preventDefault()}
         >
             {settings.uColors.map((color, index) => (
                 <Thumb
                     key={index}
                     offset={toDisplayT(color.t)}
                     background={torgb(color).split(" ")[0]}
-                    onMouseDown={() => {
+                    onMouseDown={(event) => {
+                        if (event.button != 0) return;
                         setDragged(true);
                     }}
                     onMouseEnter={(event) => {
@@ -122,6 +133,14 @@ export default function ColorGradient() {
                         setSelected({ index, element: thumbDiv });
                     }}
                     onMouseLeave={handleThumbMouseLeave}
+                    onContextMenu={(event) => {
+                        event.preventDefault();
+                        if (settings.uColors.length == 1) return;
+                        const newColors = [...settings.uColors];
+                        newColors.splice(index, 1);
+                        setSelected(undefined);
+                        setSettings({ ...settings, uColors: newColors });
+                    }}
                 />
             ))}
             {selected !== undefined && !dragged ? (
@@ -144,6 +163,7 @@ export default function ColorGradient() {
                         settings.uColors[selected.index].b = newColor.b / 255;
                     }}
                     onMouseLeave={handleThumbMouseLeave}
+                    onClick={(event) => event.preventDefault()}
                 />
             ) : (
                 <></>
